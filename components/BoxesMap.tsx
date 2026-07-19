ba1e7e5a3515efea8451856d1529842072045555
TakuyaXyze <takuya28xyze@gmail.com>
2026-07-20 00:38:32 +0900

    💄 追加のコメントを BoxesStatus 型の status プロパティに追加

--- components/BoxesMap.tsx
+++ components/BoxesMap.tsx
@@ -3,7 +3,7 @@ import { row, column } from "./GameBoard"
 type BoxesStatus = {
     rowNo: number,
     columnNo: number,
-    status: string
+    status: string      //碁石の情報。["void","white","black"]をとる。
 }
 
 const boxesMap = new Map<number, BoxesStatus>();
