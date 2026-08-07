6613a5ccb705188e4906b91e7d04ae9b6fc533c5
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-07 16:38:17 +0900

    🐛 getterが無限ループする問題を解消

--- computers/MoveCoordinate.ts
+++ computers/MoveCoordinate.ts
@@ -2,21 +2,21 @@ import { Move } from "./Evaluate"
 
 export class MoveCoordinate extends Move {
     //const position: number[][] = ; //引用元には int position; があったので一応メモ
-    private _rowNo?: number;
-    private _columnNo?: number;
+    private _rowNo: number;
+    private _columnNo: number;
     public constructor(rowNo: number, columnNo: number, value: number = 0.0) {
         super(value)
         this._rowNo = rowNo;
         this._columnNo = columnNo
     }
     get rowNo(): number {
-        return this.rowNo;
+        return this._rowNo;
     }
     set rowNo(rowNo: number) {
         this._rowNo = rowNo;
     }
     get columnNo(): number {
-        return this.columnNo;
+        return this._columnNo;
     }
     set columnNo(columnNo: number) {
         this._columnNo = columnNo;
