c5fb8b8a4081d0fd56a7e02f40d297f7bdf65ea0
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-08 16:32:45 +0900

    ✨ αβ法の導入

---
+++ computers/AlphaBetaSearch.ts
@@ -0,0 +1,75 @@
+import { BoardState } from "./BoardState";
+import { BoardTrail } from "./BoardTrail";
+import { Move, Search, State, TrailStack } from "./Evaluate"
+import { MoveCoordinate } from "./MoveCoordinate";
+
+export class AlphaBetaSearch extends Search {
+    // どの深さまで読むか?
+    private maxLevel: number = 1; //初期値は深さ1
+    // インスタンス生成時にレベルを設定
+    AlphaBetaSearch(maxLevel: number) {
+        this.maxLevel = maxLevel;
+    }
+    //alpha
+    eval(boxes: (string | null)[][], blackIsNext: boolean, currentMove: number, bstate: BoardState, level: number, alpha: number, beta: number): number {
+        // 末端のレベルでは局面の評価値。Randomではlevel==undefined
+        if (level == 0 || level == undefined) return bstate.eval();
+        else {
+            // そうでない時はベストの手の時の評価値
+            const best = this.bestMove(boxes, blackIsNext, currentMove, level, alpha, beta);
+            if (best == null) return bstate.eval();
+            return best.value;
+        }
+    }
+    bestMove(boxes: (string | null)[][], blackIsNext: boolean, currentMove: number, level: number = this.maxLevel, alpha: number, beta: number): (MoveCoordinate | null) {
+        // 可能な手を全部生成する
+        const bstate = new BoardState(boxes, blackIsNext, currentMove);
+        const moves = bstate.legalMoves(boxes);
+        let size: number = 0;
+        if (moves == undefined) return null;
+        else size = moves.length;
+        // 最良の手が複数あるのでそれを管理する
+        let bestMoves = new Array;
+        //const best: Move = null; //引用元では未使用で宣言されていた謎変数。しかも=nullはエラーになる。
+        // 最良の手の値を負の無限大に設定しておく
+        let bestVal = Number.NEGATIVE_INFINITY;
+        for (let i = 0; i < size; i++) {
+            const move = moves[i];
+            // 1手進めてみる
+            const trail: BoardTrail = bstate.doMove(move);
+            // 評価値を計算
+            /*// 1手進んだ時(相手の番)の評価値なので符号を入れ替える．
+            const moveVal: number = -this.eval(state, level - 1);*/
+            let moveVal: number;
+            if (!blackIsNext) moveVal = this.eval(boxes, blackIsNext, currentMove, bstate, level - 1, alpha, beta);
+            else moveVal = -this.eval(boxes, blackIsNext, currentMove, bstate, level - 1, -alpha, -beta);
+            // 戻す
+            bstate.undoMove(trail);
+            move.value = moveVal;
+            console.log("AlphaBetaSearch-level" + level + "-moveVal:" + moveVal);
+            // 評価値がこれまでのbestを超えた時
+            if (bestVal < moveVal) {
+                if (moveVal > beta) {
+                    return move; //枝切り
+                }
+                bestVal = moveVal;
+                if (bestVal > alpha) {
+                    alpha = bestVal;
+                }
+                bestMoves = new Array;
+                bestMoves.push(move);
+            }
+            // 評価値がこれまでのbestと同じ時
+            else if (bestVal == moveVal) {
+                bestMoves.push(move);
+            }
+        }
+        // bestの中から乱数で選択
+        const bestSize = bestMoves.length;
+        if (bestSize > 0) {
+            const selected = Math.floor(Math.random() * bestSize);
+            return bestMoves[selected];
+        }
+        else return null;
+    }
+}
---
+++ computers/PutAlphaBeta.ts
@@ -0,0 +1,13 @@
+import { AlphaBetaSearch } from "./AlphaBetaSearch";
+import { MoveCoordinate } from "./MoveCoordinate";
+
+export function computerTurnMAlphaBetaSearch(boxes: string[][], blackIsNext: boolean, currentMove: number, depth: number) {
+    console.log("computerTurnMinMaxSearch-start");
+    //setComputingTime(Date.now() - computingStartTime);
+    const move = new AlphaBetaSearch;
+    const data = move.bestMove(boxes, blackIsNext, currentMove, depth, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+    console.log("computerTurnMinMaxSearch-finish");
+    if (data === null) return null;
+    const coordinate = new MoveCoordinate(data.rowNo, data.columnNo);
+    return coordinate;
+} 
