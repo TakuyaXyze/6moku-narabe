f3507e4e936f5f07ad66cbebd23b688538bb9d40
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-13 13:44:44 +0900

    🎨 デフォルトを19×19に変更

--- components/PlayGround.tsx
+++ components/PlayGround.tsx
@@ -13,7 +13,7 @@ import { computerTurnDepth1Search } from "../computers/PutDepth1Search";
 import { computerTurnMinMaxSearch } from "../computers/PutMinMax";
 import { computerTurnAlphaBetaSearch } from "../computers/PutAlphaBeta"
 
-export const ROWS = 5;
+export const ROWS = 19;
 export const COLUMNS = ROWS;
 export let rowNos = new Array<number>;
 for (let i = 0; i < ROWS; i++) {
@@ -23,7 +23,7 @@ export let columnNos = new Array<number>;
 for (let i = 0; i < COLUMNS; i++) {
     columnNos.push(i);
 }
-export const SEQUENCE_LENGTH = 4; //MAX6
+export const SEQUENCE_LENGTH = 6; //MAX6
 
 export function PlayGround() {
 
