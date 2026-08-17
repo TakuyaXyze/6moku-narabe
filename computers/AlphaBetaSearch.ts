937ae472f80f78fb5cd6a6035755fabfefaa68f0
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-18 04:58:45 +0900

    🎨 αβ法の修正

--- computers/AlphaBetaSearch.ts
+++ computers/AlphaBetaSearch.ts
@@ -12,116 +12,106 @@ export class AlphaBetaSearch extends Search {
         super()
         this.maxLevel = maxLevel;
     }
-    eval(boxes: (string | null)[][], currentMove: number, bstate: BoardState, level: number): number {
+    eval(boxes: (string | null)[][], currentMove: number, bstate: BoardState, level: number, alpha: number, beta: number): (number | null) {
         // 末端のレベルでは局面の評価値。Randomではlevel==undefined
         console.log("start-evaluation:level=" + level);
         if (level == 0) return bstate.eval();
         else {
             // そうでない時はベストの手の時の評価値
-            const best = this.bestMove(boxes, currentMove, level);
-            if (best == null || best.value == undefined) return bstate.eval();
+            const best = this.bestMove(boxes, currentMove, level, alpha, beta);
+            if (best == null) return null;
+            if (best.value == undefined) return bstate.eval();
             console.log("eval()-end-best.value:" + best.value);
             return best.value;
         }
     }
     bestMove(boxes: (string | null)[][], currentMove: number, level: number = this.maxLevel, alpha?: number, beta?: number): (MoveCoordinate | null) {
         console.log("AlphaBetaSearch-level" + level + "-bestMove:start");
+
+        let _alpha: number;
+        if (alpha == undefined) _alpha = Number.POSITIVE_INFINITY;
+        else _alpha = alpha;
+        console.log("alphaが" + _alpha + "に更新されました");
+        let _beta: number;
+        if (beta == undefined) _beta = Number.NEGATIVE_INFINITY;
+        else _beta = beta;
+        console.log("betaが" + _beta + "に更新されました");
         const bstate = new BoardState(boxes, currentMove, level);
-        console.log("boxes:" + bstate.state + " currentMove:" + bstate.currentMove + " blackIsNext:" + checkBlackIsNext(bstate.currentMove));
+
+        console.log("currentMove:" + bstate.currentMove + " blackIsNext:" + checkBlackIsNext(bstate.currentMove));
         const moves = bstate.legalMoves(bstate.state);
         let size: number = 0;
         if (moves == null) return null;
         else size = moves.length;
         console.log("size=moves.length:" + size);
+
         // 最良の手が複数あるのでそれを管理する
         let bestMoves = new Array<MoveCoordinate>;
-        let bestValue;
+        let bestValue: (number | undefined);
         for (let i = 0; i < size; i++) {
             console.log("for文の内側開始-level" + bstate.level + ":" + (i + 1) + "回目");
-            if (alpha == undefined) {
-                alpha = Number.POSITIVE_INFINITY;
-            }
-            if (beta == undefined) {
-                beta = Number.NEGATIVE_INFINITY;
-            }
             const move = moves[i];
             // 1手進めてみる
             const trail: BoardTrail = bstate.doMove(move);
             // 評価値を計算
-            const moveValue = this.eval(bstate.state, bstate.currentMove, bstate, bstate.level);
+            const moveValue = this.eval(bstate.state, bstate.currentMove, bstate, bstate.level, _alpha, _beta);
             console.log("AlphaBetaSearch-level=" + bstate.level + " currentMove:" + bstate.currentMove + " moveVal:" + moveValue);
+            if (moveValue == null) {
+                console.log("currentMove:" + bstate.currentMove + " level=" + bstate.level + " " + (i + 1) + "回目 continue;")
+                continue;
+            }
             move.value = moveValue;
             const blackIsThisTurn = checkBlackIsNext(bstate.currentMove - 1);
             const blackIsPreviousTurn = checkBlackIsNext(bstate.currentMove - 2);
-            if (!blackIsThisTurn) {
+            if (!blackIsThisTurn) { //whiteIsThisTurn
                 console.log("currentMove=" + bstate.currentMove + " whiteIsThisTurn");
                 if (bestValue == undefined) {
                     bestValue = Number.NEGATIVE_INFINITY;
                     console.log("bestValueが" + bestValue + "に更新されました")
                 }
-                if (!blackIsPreviousTurn) {
-                    if (moveValue > bestValue) {
-                        bestValue = moveValue;
-                        bestMoves = new Array<MoveCoordinate>;
-                        bestMoves.push(move);
+                if (moveValue > bestValue) {
+                    bestValue = moveValue;
+                    _beta = bestValue;
+                    if (blackIsPreviousTurn && bestValue > _alpha) {
+                        console.log("\n\n\n\n\n\nαカット\n\n\n\n\n\n");
+                        // 戻す
+                        bstate.undoMove(trail);
+                        return move;
                     }
-                    // 評価値がこれまでのbestと同じ時
-                    else if (moveValue == bestValue) {
-                        bestMoves.push(move);
-                    }
+                    bestMoves = new Array<MoveCoordinate>;
+                    bestMoves.push(move);
                 }
-                else {
-                    if (moveValue > bestValue) {
-                        bestValue = moveValue;
-                        if (bestValue > alpha) {
-                            console.log("αカット");
-                            return move;
-                        }
-                        bestMoves = new Array<MoveCoordinate>;
-                        bestMoves.push(move);
-                    }
-                    // 評価値がこれまでのbestと同じ時
-                    else if (moveValue == bestValue) {
-                        bestMoves.push(move);
-                    }
+                // 評価値がこれまでのbestと同じ時
+                else if (moveValue == bestValue) {
+                    bestMoves.push(move);
                 }
             }
-            else {
+            else { //blackIsThisTurn
                 console.log("currentMove=" + bstate.currentMove + " blackIsThisTurn");
                 if (bestValue == undefined) {
                     bestValue = Number.POSITIVE_INFINITY;
                     console.log("bestValueが" + bestValue + "に更新されました")
                 }
-                if (blackIsPreviousTurn) {
-                    if (moveValue < bestValue) {
-                        bestValue = moveValue;
-                        bestMoves = new Array<MoveCoordinate>;
-                        bestMoves.push(move);
+                if (moveValue < bestValue) {
+                    bestValue = moveValue;
+                    _alpha = bestValue;
+                    if (!blackIsPreviousTurn && bestValue < _beta) {
+                        console.log("\n\n\n\n\n\nβカット\n\n\n\n\n\n");
+                        // 戻す
+                        bstate.undoMove(trail);
+                        return move;
                     }
-                    // 評価値がこれまでのbestと同じ時
-                    else if (moveValue == bestValue) {
-                        bestMoves.push(move);
-                    }
-                } else {
-                    if (moveValue < bestValue) {
-                        bestValue = moveValue;
-                        if (bestValue < beta) {
-                            console.log("βカット");
-                            return move;
-                        }
-                        bestMoves = new Array<MoveCoordinate>;
-                        bestMoves.push(move);
-                    }
-                    // 評価値がこれまでのbestと同じ時
-                    else if (moveValue == bestValue) {
-                        bestMoves.push(move);
-                    }
+                    bestMoves = new Array<MoveCoordinate>;
+                    bestMoves.push(move);
+                }
+                // 評価値がこれまでのbestと同じ時
+                else if (moveValue == bestValue) {
+                    bestMoves.push(move);
                 }
             }
             // 戻す
             bstate.undoMove(trail);
-            console.log("bestValue:" + bestValue);
-            console.log("for文の内側終了");
+            console.log("for文の内側終了-bestValue:" + bestValue);
         }
         // bestの中から乱数で選択
         const bestSize = bestMoves.length;
@@ -136,3 +126,54 @@ export class AlphaBetaSearch extends Search {
         else return null;
     }
 }
+
+/*
+
+自分=コンピュータ・後手・白
+相手=プレイヤー・先手・黒
+
+αカット: 得点の下限が定まっているときの枝刈り
+現在が自分の手番で、
+直前が相手の手番のときに、
+自分の手番を打った直後の評価値がそのときのα値を超えていたときに発動
+→黒→白
+
+α値: 相手が最善手を打った直後の評価値
+相手の手番を打った直後の評価値の最小値
+
+βカット: 得点の上限が定まっているときの枝刈り
+現在が相手の手番で、
+直前が自分の手番のときに、
+相手の手番を打った直後の評価値がそのときのβ値を下回っていたときに発動
+→白→黒
+
+β値: 自分が最善手を打った直後の評価値
+自分の手番を打った直後の評価値の最大値
+
+*/
+
+/*
+
+01 黒
+
+02 白 
+03 黒
+04 黒
+
+05 白
+06 白
+07 黒
+
+08 黒
+
+09 白
+10 白
+11 黒
+
+12 黒
+
+13 白
+14 白
+15 黒
+
+*/
