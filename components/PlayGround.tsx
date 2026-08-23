55e373f471e50d13d47ffcc7168670b6434a353a
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-23 10:22:06 +0900

    ✨ UIの追加（処理中の状態表示、処理時間表示、ゲームモード選択ボタン説明、巻き戻しボタン説明）

--- components/PlayGround.tsx
+++ components/PlayGround.tsx
@@ -13,6 +13,7 @@ import { computerTurnDepth1Search } from "../computers/PutDepth1Search";
 import { computerTurnMinMaxSearch } from "../computers/PutMinMax";
 import { computerTurnAlphaBetaSearch } from "../computers/PutAlphaBeta"
 import { computerTurnBeamSearch } from "../computers/PutBeam";
+import { ClockLoader } from "react-spinners";
 
 export const ROWS = 19;
 export const COLUMNS = ROWS;
@@ -30,7 +31,7 @@ export function PlayGround() {
 
     const [history, setHistory] = useState([Array(ROWS).fill(null).map(() => Array<(string | null)>(COLUMNS).fill(null))]);
     const [currentMove, setCurrentMove] = useState(0);
-    const [currentGameMode, setcurrentGameMode] = useState("Random");
+    const [currentGameMode, setcurrentGameMode] = useState("Beam-depth4");
 
     function handlePlay(nextBoxes: (string | null)[][]): void {
         const nextHistory = [...history.slice(0, currentMove + 1), nextBoxes];
@@ -71,44 +72,51 @@ export function PlayGround() {
         handlePlayDouble(nextBoxes);
     }
 
+
+    //処理時間の計測
+    const [computingTime, setComputingTime] = useState(0);
+    const [sumTime, setSumTime] = useState(0);
+
     useEffect(() => {
         const blackIsNext = checkBlackIsNext(currentMove);
         if (blackIsNext) return;
         if (detectSequence(history[currentMove], "b")[SEQUENCE_LENGTH - 2] > 0) return;
         if (detectSequence(history[currentMove], "w")[SEQUENCE_LENGTH - 2] > 0) return;
-        setTimeout(() => { computerTurn() }, 300)
-    }, [history])
-
-    //処理時間の計測
-    //const [computingStartTime, setComputingStartTime] = useState(Date.now)
-    //const [computingTime, setComputingTime] = useState(0)
+        setTimeout(() => {
+            const computingStartTime = Date.now();
+            computerTurn();
+            const time = Date.now() - computingStartTime;
+            setComputingTime(time);
+            setSumTime(a => a + time);
+        }, 300)
+    }, [history, currentMove])
 
     function computerTurn(): void {
-        //setComputingStartTime(Date.now);
+
         switch (currentGameMode) {
-            case "Random":
+            case "Random-depth":
                 computerTurnWithResult(computerTurnRandom(history[currentMove], currentMove));
                 break;
             case "Depth1Search":
                 computerTurnWithResult(computerTurnDepth1Search(history[currentMove], currentMove));
                 break;
-            case "MinMax3":
+            case "MinMax-depth3":
                 computerTurnWithResult(computerTurnMinMaxSearch(history[currentMove], currentMove, 3));
                 break;
-            case "MinMax6":
+            case "MinMax-depth6":
                 computerTurnWithResult(computerTurnMinMaxSearch(history[currentMove], currentMove, 6));
                 break;
-            case "AlphaBeta3":
+            case "AlphaBeta-depth3":
                 computerTurnWithResult(computerTurnAlphaBetaSearch(history[currentMove], currentMove, 3));
                 break;
-            case "AlphaBeta6":
+            case "AlphaBeta-depth6":
                 computerTurnWithResult(computerTurnAlphaBetaSearch(history[currentMove], currentMove, 6));
                 break;
-            case "Beam4":
-                computerTurnWithDoubleResult(computerTurnBeamSearch(history[currentMove], currentMove, 8, 4));
+            case "Beam-depth4":
+                computerTurnWithDoubleResult(computerTurnBeamSearch(history[currentMove], currentMove, 10, 4));
                 break;//引数_3は、ビームサーチで次の深度に持ち出す場合の数。上位n個のみが次の深度について検証される
-            case "Beam6":
-                computerTurnWithDoubleResult(computerTurnBeamSearch(history[currentMove], currentMove, 8, 6));
+            case "Beam-depth6":
+                computerTurnWithDoubleResult(computerTurnBeamSearch(history[currentMove], currentMove, 10, 6));
                 break;//引数_3は、ビームサーチで次の深度に持ち出す場合の数。上位n個のみが次の深度について検証される
             default:
                 throw new Error("GameModeが指定されていません");
@@ -129,24 +137,7 @@ export function PlayGround() {
 
     function jumpTo(nextMove: number) {
         setCurrentMove(nextMove);
-        const blackIsNext = checkBlackIsNext(currentMove);
-        if (blackIsNext) return;
-        if (detectSequence(history[currentMove], "b")[SEQUENCE_LENGTH - 2] > 0) return;
-        if (detectSequence(history[currentMove], "w")[SEQUENCE_LENGTH - 2] > 0) return;
-        computerTurn();
     }
-
-    const moves = history.map((boxes: (string | null)[][], move: number) => {
-        let description;
-        if (move > 0) {
-            description = 'Go to move #' + move;
-        } else {
-            description = 'Go to game start';
-        }
-        return (
-            <button key={move} onClick={() => jumpTo(move)}>{description}</button>
-        );
-    });
 
     let status;
     if (detectSequence(history[currentMove], "b")[SEQUENCE_LENGTH - 2] > 0) {
@@ -159,13 +150,104 @@ export function PlayGround() {
         status = 'Next player: ' + (checkBlackIsNext(currentMove) ? 'black' : 'white');
     }
 
+
+    const blackIsNext = checkBlackIsNext(currentMove);
+
+    const moves = history.map((boxes: (string | null)[][], move: number) => {
+
+        const moveBlackIsNext = checkBlackIsNext(move);
+        const moveBlackIsThisTurn = checkBlackIsNext(move - 1);
+
+        let isHoveredOnButton = false;
+
+        function setIsHoveredOnButton(boolean: boolean) {
+            isHoveredOnButton = boolean;
+        }
+
+        if (move == 0) {
+            return (
+                <button key={move}
+                    onClick={() => jumpTo(move)}
+                    disabled={!blackIsNext}
+                >Go to game start</button>
+            );
+        } else if (move === history.length - 1) return;
+        else if (!moveBlackIsNext && !moveBlackIsThisTurn) {
+            return (
+                <button key={move}
+                    onClick={() => jumpTo(move - 1)}
+                    disabled={!blackIsNext}
+                >Go to move # {move}</button>
+            );
+        }
+        else {
+            return (
+                <button key={move}
+                    onClick={() => jumpTo(move)}
+                    disabled={!blackIsNext}
+                >Go to move # {move}</button>
+            );
+        }
+
+    });
+
+    function printTimer(time: number): string {
+        let word: string;
+        let minute = Math.floor((time * 0.001) / 60);
+        let minuteString;
+        if (minute >= 10) minuteString = String(minute);
+        else if (minute > 0) minuteString = "0" + String(minute);
+        else minuteString = "00"
+        let sec = Math.floor((time * 0.001) % 60);
+        let secString;
+        if (sec >= 10) secString = String(sec);
+        else if (sec > 0) secString = "0" + String(sec);
+        else secString = "00"
+        let millisec = time - Math.floor(time * 0.001) * 1000;
+        let millisecString
+        if (millisec >= 100) millisecString = String(millisec);
+        else if (millisec >= 10) millisecString = "00" + String(millisec);
+        else if (millisec > 0) millisecString = "0" + String(millisec);
+        else millisecString = "000"
+        word = minuteString + ":" + secString + "." + millisecString;
+        return word;
+    }
+    let printComputingTime = printTimer(computingTime);
+    let printSumTime = printTimer(sumTime);
+
+    const [isInforming, setIsInforming] = useState(false);
+
     return (
         <div className="play-ground">
             <GameBoard boxes={history[currentMove]} handleClick={handleClick} />
             <div className="game-info">
-                <div>Mode:{currentGameMode}</div>
+                <div>GameMode: {currentGameMode}</div>
                 <SelectGameMode handleGameMode={handleGameMode} />
-                <div className="status">{status}</div>
+                <div className="status">
+                    {status}
+                    <ClockLoader className="loader" loading={!blackIsNext} size={24} color="#539fed" />
+                </div>
+                <div className="computing-time">
+                    <div>処理時間: {printComputingTime} s</div>
+                    <div>累積時間: {printSumTime} s</div>
+                </div>
+                <div className="moves-button-info">
+                    <img src="information.png"
+                        alt="i"
+                        width="16"
+                        onMouseEnter={() => setIsInforming(true)}
+                        onMouseLeave={() => setIsInforming(false)}
+                    />
+                    {isInforming && (
+                        <div className="information">
+                            <ul>
+                                <li>過去の手番に巻き戻し</li>
+                                <li>自分の手番時 もう一度打ち直し</li>
+                                <li>相手の手番時 2手同時自動打ち直し</li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
                 <ol className="move-info">{moves}</ol>
             </div>
         </div >
