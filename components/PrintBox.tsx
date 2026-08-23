c85ee38ee3f0ca0909a0bebe6819685586928990
TakuyaXyze <takuya28xyze@gmail.com>
2026-08-23 10:23:29 +0900

    💄 碁盤の模様を実際の碁盤の画像に差し替え

--- components/PrintBox.tsx
+++ components/PrintBox.tsx
@@ -13,7 +13,6 @@ export function PrintBox({ rowNo, columnNo, value, onBoxClick }: Props) {
         <div key={countNum} className={`stone ${value === "b" ? 'black' : ''}${value === "w" ? 'white' : ''}`}>
             <div onClick={onBoxClick}
                 className="box">
-                {rowNo + 1}-{columnNo + 1}
             </div>
         </div>
     );
